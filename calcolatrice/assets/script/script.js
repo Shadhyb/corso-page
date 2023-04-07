99a20a107e854663d06b3441760a73b4843edc5f
Shadhyb <pietro.rimoli89@gmail.com>
2023-04-07 12:34:32 +0200

    fixed calcolatrice

--- calcolatrice/assets/script/script.js
+++ calcolatrice/assets/script/script.js
@@ -1,20 +1,25 @@
 function n(dato){ 
     document.getElementById("ops").value += dato;
-    
- }
+    if(dato === 0+0+n){
+        dato = n
+    }
+    document.getElementsByClassName('output')[0].innerHTML += dato ;
+  
+}
+
+
 
  
  function ops() { 
  Math.round(Math.floor( document.getElementById("ops").value = eval(document.getElementById('ops').value))); 
- const inputElement = document.getElementById('ops');
-  const result = Math.round(Math.floor(eval(inputElement.value)));
-  const outputElement = document.getElementsByClassName('output');
-  outputElement.textContent += `${inputElement.value} = ${result}\n`;
-  inputElement.value = result;
+let ops = (Math.round(Math.floor( document.getElementById("ops").value = eval(document.getElementById('ops').value))))
+document.getElementsByClassName('output')[0].innerHTML += '=' + ops + '<br>';
+
 }
 
 function del() { 
     document.getElementById("ops").value = ""; 
+    
 }
 function phi(){
     document.getElementById('ops').value *=3.14
@@ -22,6 +27,8 @@ function phi(){
 function percentage(){
     document.getElementById('ops').value /=100 
 }
+
+
 
 
 
